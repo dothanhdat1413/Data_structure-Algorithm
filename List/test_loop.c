869fd49a541534fe59af91e07b0cc690d192cb6d
dothanhdat1413 <datdo2525@gmail.com>
2025-01-02 15:41:42 +0700

    Assignment W17 Graph

---
+++ List/test_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
+
+typedef struct List
+{
+    Node *begin;
+    Node *end;
+} List;
+
+List L
+int main() {
+    
+}
